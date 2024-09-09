e6049c0450d3cb0647d4560e291de6ff17fa3218
TimofeevDmitry76 <Timofey-76@mail.ru>
2024-09-09 20:36:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ name = 'Dmitry'
 print('Имя:',name)
 age = 48
 print('Возраст:',age)
-age = 48 + 10
-print('Новый возраст:',age)
-is_student = 'True'
+New_age = 10
+print('Новый возраст:',New_age + age)
+is_student = True
 print(is_student)
